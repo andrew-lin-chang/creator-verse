4fa5a260963304b52425030c45401fd194fbf2b3
Chang <andrewchang@utexas.edu>
2023-08-21 23:19:25 -0500

    view all creators

--- src/pages/ShowCreators.jsx
+++ src/pages/ShowCreators.jsx
@@ -1,11 +1,18 @@
 import Card from "../components/Card";
 
-export default function ShowCreators({creators}) {
-
+export default function ShowCreators({ creators }) {
   return (
     <div>
       <h1>Show Creators</h1>
-      {(creators === null || creators.length === 0) && <h2>Looks like no one is here...</h2>}
+      <div className="grid grid-cols-2 gap-4">
+        {creators === null || creators.length === 0 ? (
+          <h2>Looks like no one is here...</h2>
+        ) : (
+          creators.map((creator, index) => (
+            <Card key={index} {...creator} />
+          ))
+        )}
+      </div>
     </div>
   );
 }
