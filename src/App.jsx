5092c4796e9d75b4be741352614a7ac0dbf2b099
Chang <andrewchang@utexas.edu>
2023-08-21 23:04:54 -0500

    add basic app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
-import { useState } from 'react'
-import './App.css'
+import { useState, useEffect } from "react";
+import Layout from "./Layout";
+import ShowCreators from "./pages/ShowCreators";
+import { supabase } from "./client";
 
 function App() {
+  const [creators, setCreators] = useState([]);
+
+  useEffect(() => {
+    getCreators();
+  }, []);
+
+  async function getCreators() {
+    const { data } = await supabase.from("creators").select();
+    console.log(data);
+    setCreators(data);
+  }
 
   return (
     <>
-
+      <Layout>
+        <h1 className="font-bold text-4xl tracking-tight">CreatorVerse</h1>
+        <ShowCreators creators={creators} />
+      </Layout>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
