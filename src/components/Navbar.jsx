6c13286547fe37505c3f12ad59d11237bcf47b53
Chang <andrewchang@utexas.edu>
2023-08-24 12:17:04 -0500

    navbar link home

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,13 @@
+import { Link } from "react-router-dom";
+
 export default function Navbar() {
   return (
-    <div className="flex bg-dark sticky justify-center items-center p-2 text-white">
-      <h1 className="font-bold text-4xl">
-        Creator<span className="text-primary">verse</span>
-        
-      </h1>
+    <div className="flex gap-2 bg-dark sticky justify-center items-center p-4 text-white">
+      <Link to="/">
+        <h1 className="font-bold text-4xl">
+          creator<span className="text-primary">verse</span>
+        </h1>
+      </Link>
     </div>
   );
 }
