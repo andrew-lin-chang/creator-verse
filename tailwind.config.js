728015878ff95f6065f39467176e75811b60fd46
Chang <andrewchang@utexas.edu>
2023-08-24 12:05:22 -0500

    add color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark: "#363636",
+        base: "#3f4042",
+        primary: "#c3352e",
+        secondary: "#ac362e",
+      },
+    },
   },
   plugins: [],
-}
-
+};
