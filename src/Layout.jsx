5a5de106d128cce75e520f54d5e88f8b8dd65128
Chang <andrewchang@utexas.edu>
2023-08-27 12:03:57 -0500

    move layout

--- src/Layout.jsx
+++
@@ -1,8 +0,0 @@
-
-export default function Layout({children}) {
-  return (
-    <div className="mx-auto max-w-7xl px-8 text-white">
-      {children}
-    </div>
-  )
-}
--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import "./index.css";
 
 import { BrowserRouter } from "react-router-dom";
-import Layout from "./Layout.jsx";
+import Layout from "./components/Layout.jsx";
 import Navbar from "./components/Navbar.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
