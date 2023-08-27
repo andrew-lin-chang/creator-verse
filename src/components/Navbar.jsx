1ea0231115d8fae207d7f6e446f266fdd62e6a88
Chang <andrewchang@utexas.edu>
2023-08-27 12:18:41 -0500

    change app name

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar() {
     <div className="flex gap-2 bg-dark sticky justify-center items-center p-4 text-white">
       <Link to="/">
         <h1 className="font-bold text-4xl">
-          creator<span className="text-primary">verse</span>
+          youtuber<span className="text-primary">verse</span>
         </h1>
       </Link>
     </div>
